1ebe4ed8424e4fbdf1ddb478cdef9cee6c368f0d
h2yhm <heri2000@yahoo.com>
2023-11-29 22:00:41 +0700

    Implement verifyThreadAvailability in ThreadRepository

--- src/Domains/threads/ThreadRepository.js
+++ src/Domains/threads/ThreadRepository.js
@@ -2,6 +2,10 @@ class ThreadRepository {
   async addThread(userId, addThread) {
     throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   }
+
+  async verifyThreadAvailability(threadId) {
+    throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
   
   async getThreadById(threadId) {
     throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
--- src/Domains/threads/_test/ThreadRepository.test.js
+++ src/Domains/threads/_test/ThreadRepository.test.js
@@ -9,6 +9,10 @@ describe('ThreadRepository interface', () => {
     await expect(threadRepository.addThread('', {})).rejects.toThrowError(
       'THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED'
     );
+    
+    await expect(threadRepository.verifyThreadAvailability('')).rejects
+      .toThrowError('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+
     await expect(threadRepository.getThreadById('')).rejects.toThrowError(
       'THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED'
     );
