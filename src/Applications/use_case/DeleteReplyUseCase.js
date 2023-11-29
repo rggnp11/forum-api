e77982444eb4f3188fa1e081ab1f3ef0581b0b9f
h2yhm <heri2000@yahoo.com>
2023-11-30 01:54:29 +0700

    Check reply availability and reply owner in DeleteReplyUseCase

--- src/Applications/use_case/DeleteReplyUseCase.js
+++ src/Applications/use_case/DeleteReplyUseCase.js
@@ -1,9 +1,25 @@
+const NotFoundError = require('../../Commons/exceptions/NotFoundError');
+const AuthorizationError = require('../../Commons/exceptions/AuthorizationError');
+
 class DeleteReplyUseCase {
   constructor({ replyRepository }) {
     this._replyRepository = replyRepository;
   }
 
   async execute(userId, threadId, parentId, replyId) {
+    const isReplyAvailable = await this._replyRepository
+      .verifyReplyAvailability(replyId);
+    if (!isReplyAvailable) {
+      throw new NotFoundError('balasan tidak ditemukan atau tidak valid');
+    }
+
+    const isReplyOwner = await this._replyRepository.verifyReplyOwner(
+      userId, replyId
+    );
+    if (!isReplyOwner) {
+      throw new AuthorizationError('user bukan owner dari balasan');
+    }
+
     await this._replyRepository.deleteReplyById(
       userId, threadId, parentId, replyId
     );
--- src/Applications/use_case/_test/DeleteReplyUseCase.test.js
+++ src/Applications/use_case/_test/DeleteReplyUseCase.test.js
@@ -1,10 +1,55 @@
 const ReplyRepository = require('../../../Domains/replies/ReplyRepository');
 const DeleteReplyUseCase = require('../DeleteReplyUseCase');
+const NotFoundError = require('../../../Commons/exceptions/NotFoundError');
+const AuthorizationError = require('../../../Commons/exceptions/AuthorizationError');
 
 describe('DeleteReplyUseCase', () => {
+  it('should throw NotFoundError when reply not exist', async () => {
+    // Arrange
+    const mockReplyRepository = new ReplyRepository();
+    mockReplyRepository.verifyReplyAvailability = jest.fn()
+      .mockImplementation(() => Promise.resolve(false));
+
+    const deleteReplyUseCase = new DeleteReplyUseCase({
+      replyRepository: mockReplyRepository,
+    });
+
+    // Action & Assert
+    expect(deleteReplyUseCase.execute(
+      'user-123', 'thread-123', 'comment-123', 'reply-123'
+    )).rejects
+      .toThrowError(NotFoundError);
+  });
+
+  it(
+    'should throw AuthorizationError when user is not reply owner',
+    async () => {
+      // Arrange
+      const mockReplyRepository = new ReplyRepository();
+      mockReplyRepository.verifyReplyAvailability = jest.fn()
+        .mockImplementation(() => Promise.resolve(true));
+      mockReplyRepository.verifyReplyOwner = jest.fn()
+        .mockImplementation(() => Promise.resolve(false));
+
+      const deleteReplyUseCase = new DeleteReplyUseCase({
+        replyRepository: mockReplyRepository,
+      });
+  
+      // Action & Assert
+      expect(deleteReplyUseCase.execute(
+        'user-123', 'thread-123', 'comment-123', 'reply-123'
+      )).rejects
+        .toThrowError(AuthorizationError);
+    }
+  );
+  
   it('should orchestrating the delete reply action correctly', async () => {
     // Arrange
     const mockReplyRepository = new ReplyRepository();
+    mockReplyRepository.verifyReplyAvailability = jest.fn()
+      .mockImplementation(() => Promise.resolve(true));
+    mockReplyRepository.verifyReplyOwner = jest.fn()
+      .mockImplementation(() => Promise.resolve(true));
     mockReplyRepository.deleteReplyById = jest.fn()
       .mockImplementation(() => Promise.resolve());
     
