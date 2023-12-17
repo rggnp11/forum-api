3ddcef0c941ad7a4fe2a58984621eea9389a17f6
h2yhm <heri2000@yahoo.com>
2023-12-17 23:52:06 +0700

    Implement putLikeHandler

--- src/Interfaces/http/api/likes/handler.js
+++ src/Interfaces/http/api/likes/handler.js
@@ -2,9 +2,23 @@ const ToggleLikeUseCase = require('../../../../Applications/use_case/ToggleLikeU
 
 class LikesHandler {
   constructor(container) {
+    this._container = container;
+
+    this.putLikeHandler = this.putLikeHandler.bind(this);
   }
 
   async putLikeHandler(request, h) {
+    const toggleLikeUseCase = this._container.getInstance(
+      ToggleLikeUseCase.name
+    );
+    const { threadId, commentId } = request.params;
+    const { userId } = request.auth.credentials;
+
+    await toggleLikeUseCase.execute(userId, threadId, commentId);
+
+    const response = h.response({ status: 'success' });
+    response.code(200);
+    return response;
   }
 }
 
